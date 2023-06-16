c6e8b6188e3f731edffed986c71a39bf36f0cc3b
roalwh <nmnmnmlk6248@gmail.com>
2023-06-16 17:30:13 +0900

    20230616

--- day5/react-calendar/src/App.jsx
+++ day5/react-calendar/src/App.jsx
@@ -5,17 +5,37 @@ import Header from './components/Header'
 import Calendar from './components/Calendar'
 
 function App() {
-const [year,setyear] = useState(new Date().getFullYear());
-const [month,setmonth] = useState(new Date().getMonth());
-const [date,setdate] = useState(new Date().getDate());
-console.log(year,month,date )
+  const d = new Date();
+  const [year, setyear] = useState(d.getFullYear());
+  const [month, setmonth] = useState(d.getMonth());
+  const [date, setdate] = useState(d.getDate());
+  const today = `${d.getFullYear()}${d.getMonth()}${d.getDate()}`;
+  console.log(today);
 
-
+  const nextMonth =()=>{
+    if(month<11){
+      return setmonth(month+1);
+    }else{
+      return setmonth(0) & setyear(year+1); 
+    }
+    
+  }
+  const prevMonth =()=>{
+    if(month>0){
+      return setmonth(month-1);
+    }else{
+      return setmonth(11) & setyear(year-1); 
+    }
+  }
 
   return (
     <div className="app">
       <Header></Header>
-      <Calendar year={year} month={month} date={date}></Calendar>
+      <Calendar year={year} month={month} date={date} today={today}></Calendar>
+      <div className="btn-group">
+        <button className="prev" onClick={prevMonth}>이전</button>
+        <button className="next" onClick={nextMonth}>다음</button>
+      </div>
     </div>
   )
 }
--- day5/react-calendar/src/components/Calendar.jsx
+++ day5/react-calendar/src/components/Calendar.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function Calendar(props) {
-    console.log(props.year);
     // 날짜
     let d = new Date();
     // let day = d.getDay();//요일
@@ -9,14 +8,16 @@ function Calendar(props) {
     // let month = 1 + d.getMonth(); //월 0부터시작하기에 1을 더해줌
     // let date = d.getDate(); //일
     
-    let {year, month, date} = props;
+    let {year, month, date, today} = props;
 
 
     // 아래와 같이 0을 넣으면 해당 달의 마지막 날짜 값이 나온다
     let lastDate = new Date(year, month, 0).getDate();
     // 시작일
     let startDate = new Date(year, month-1, 1).getDay();
-  
+    let setToDay = `${year}${month}`
+    console.log(today);
+    console.log(setToDay);
 
     function tablelist() {
         console.log(Array(lastDate).fill().map((_, i) => i + 1));
@@ -26,7 +27,7 @@ function Calendar(props) {
         <div className="calender">
             <header>
                 <h1>{year}년</h1>
-                <h2>{month}월</h2>
+                <h2>{month+1}월</h2>
             </header>
             <main>
                 <ul className="date">
@@ -39,8 +40,10 @@ function Calendar(props) {
                 }
                     {
                         Array(lastDate).fill().map((_, i) => {
+                            console.log("today : "+today);
+                            console.log("setToDay : "+setToDay+i);
                             return (
-                                date===i+1 ? <li style={{backgroundColor:'red'}} key={i}>{i + 1}</li> : <li key={i}>{i + 1}</li>
+                                today===setToDay+(i+1) ? <li style={{backgroundColor:'red'}} key={i}>{i + 1}</li> : <li key={i}>{i + 1}</li>
                                 
                             )
                         })
